d7b12a8d1de616de209dfb7bf721e8a86a05b128
roman015 <xaviervarricatt@live.in>
2021-10-31 19:33:32 +0800

    TestSignalRHub Added

---
+++ Clients/TestSignalRHub.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman015API.Clients
+{
+    public class TestSignalRHub
+    {
+        private static HubConnection hubConnection;
+        private static TestSignalRHub testSignalRHub;
+
+        private TestSignalRHub() { }
+
+        public static TestSignalRHub GetSingletonInstance()
+        {
+            if (testSignalRHub == null)
+            {
+                testSignalRHub = new TestSignalRHub();
+            }
+
+            if (hubConnection == null)
+            {
+                hubConnection = new HubConnectionBuilder()
+                  .WithUrl("https://api.roman015.com/NotificationHub")
+                  .WithAutomaticReconnect()
+                  .Build();
+
+                hubConnection.Closed += ((arg) =>
+                {
+                    testSignalRHub.Closed?.Invoke(arg);
+                    return hubConnection.StartAsync();
+                });
+                hubConnection.On<string>("TestMessage", message =>
+                {
+                    testSignalRHub.TestMessage?.Invoke(message);
+                });
+            }            
+
+            return testSignalRHub;
+        }
+
+        public event Func<Exception, Task> Closed;
+        public event Func<string, Task> TestMessage;
+        public async void StartConnection()
+        {
+            await hubConnection.StartAsync();
+        }
+    }
+}
